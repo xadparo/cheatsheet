b41ae254f514c092fc5c0fc50ccfb701d9086567
xadparo <45482148+xadparo@users.noreply.github.com>
2021-09-07 19:23:11 +0900

    feat: add defer

---
+++ src/defer.ts
@@ -0,0 +1,26 @@
+export class Defer {
+  /** 미뤄진 함수 배열 */
+  deferreds: Function[] = []
+  push(deferred: Function) {
+    this.deferreds.push(deferred)
+    return this
+  }
+  /** 미뤄진 함수들을 역순으로 실행 */
+  async resolve() {
+    for (let idx = this.deferreds.length - 1; idx >= 0; idx--) {
+      const deferred = this.deferreds[idx]
+      await deferred()
+    }
+  }
+}
+
+type Box<T> = Promise<T> | T
+export async function DeferContext<T = any>(func: (push: Function) => Box<T>) {
+  const defer = new Defer
+
+  const result = await func(defer.push.bind(defer))
+
+  await defer.resolve()
+
+  return result
+}
